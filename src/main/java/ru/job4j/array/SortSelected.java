bb026421e552c17f3f8c9f3cfeb3c753ba2e8730
Elvira Semenova <shalopayka@gmail.com>
2020-12-04 15:02:16 +0300

    Сортировка с выборкой

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,17 @@
+
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            if (i != index) {
+                int temp = data[i];
+                data[i] = data[index];
+                data[index] = temp;
+            }
+        }
+        return data;
+    }
+}
