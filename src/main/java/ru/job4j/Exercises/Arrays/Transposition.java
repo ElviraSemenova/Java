8ef688de5dd7e88b25bbc5ede5c45a249eaf3840
Elvira Semenova <shalopayka@gmail.com>
2020-12-04 21:54:21 +0300

    Транспортировать матрицу

---
+++ src/main/java/ru/job4j/Exercises/Arrays/Transposition.java
@@ -0,0 +1,15 @@
+package ru.job4j.Exercises.Arrays;
+
+public class Transposition {
+    public static int[][] convert(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
