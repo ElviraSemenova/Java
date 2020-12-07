1a0745702b085d0d7e2bc2cc1580928a045b324e
Elvira Semenova <shalopayka@gmail.com>
2020-12-07 19:03:32 +0300

    1.7. Состояние объекта [#410786]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int value;
+
+    public Battery(int size) {
+        this.value = size;
+    }
+
+    public void exchange(Battery another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+        first.exchange(second);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+
+    }
+}
