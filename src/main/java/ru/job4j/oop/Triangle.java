39dea319054fe0c1ca9036135912e0bf67c7d38f
Elvira Semenova <shalopayka@gmail.com>
2020-12-07 21:04:47 +0300

    2.2. Рефакторинг - Площадь треугольника. [#410777]

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point first, Point second, Point third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac);
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return rsl;
+    }
+}
