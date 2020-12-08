b03b2d148cb052eef5ff2fb48ee6e8339930ea16
Elvira Semenova <shalopayka@gmail.com>
2020-12-08 21:05:40 +0300

    6. Почему наследование - это плохо [#410795]

---
+++ src/main/java/ru/job4j/oop/Shop.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product product = of("Oil", 100);
+        System.out.println(product.label());
+    }
+
+    public static Product of(String name, int price) {
+        return new Product(name, price);
+    }
+}
