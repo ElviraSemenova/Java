4d96afc49186c0634945dae10e5a896081b5c8ce
Elvira Semenova <shalopayka@gmail.com>
2020-12-10 11:42:50 +0300

    3.1. Массив с пустыми ячейками [#410803]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -1,19 +1,15 @@
 package ru.job4j.pojo;
 
 public class Shop {
-    public static void main(String[] args) {
-        Product products[] = new Product[5];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        products[2] = new Product("Egg", 19);
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
         for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            if (product == null) {
-                System.out.println(i);
+            if (products[i] == null) {
+                rsl = i;
                 break;
             }
         }
-
+        return rsl;
     }
 }
 
