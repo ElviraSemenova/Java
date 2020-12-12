b77d8a82a2dfd72915a93020d7359486efcd5aee
Elvira Semenova <shalopayka@gmail.com>
2020-12-12 13:01:01 +0300

    2. Scanner и чтение числа из консоли. [#410730]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -6,15 +6,12 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int matches = 11;
-        Boolean player1 = true;
-        String username = "Первый игрок";
+        boolean player1 = true;
+        String username = " ";
         while (matches > 0) {
-            if (player1)
-                System.out.println(username);
-            else {
-                System.out.println("Второй игрок, ");
-            }
-            System.out.print("заберите от 1 до 3х спичек, выиигрывает тот, кто заберет последнюю: ");
+            username = player1 ? "Первый игрок" : "Второй игрок";
+            System.out.println("Ход совершает: " + username);
+            System.out.print("заберите от 1 до 3х спичек, выигрывает тот, кто заберет последню: ");
             int select = Integer.valueOf(input.nextLine());
             if (select < 1 || select > 3) {
                 System.out.println("Вы выбрали неверное количество");
@@ -23,12 +20,10 @@ public class Matches {
                 if (matches > 0) {
                     player1 = !player1;
                     System.out.println("Осталось " + matches);
-                } else
-                    System.out.println("Осталось 0 спичек. " + username + "," + " Вы выйграли!");
+                }
             }
         }
+        System.out.println("Осталось 0 спичек. " + username + "," + " Вы выиграли!");
     }
 }
-
-
 
