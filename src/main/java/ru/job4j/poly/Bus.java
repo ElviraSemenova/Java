e25912f27c83c94db2b033cf30d8fa6751bc53f2
Elvira Semenova <shalopayka@gmail.com>
2020-12-12 10:45:13 +0300

    3. Интерфейс Input [#410731]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int quantity) {
+
+    }
+
+    @Override
+    public int fillUp(int quantity) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void ride();
+
+    void passengers(int quantity);
+
+    int fillUp(int quantity);
+}
+
+
