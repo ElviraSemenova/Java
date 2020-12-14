04755891269657da05921e207f1b3c7f9097abb4
Elvira Semenova <shalopayka@gmail.com>
2020-12-14 20:10:56 +0300

    0.4. Пользовательские исключения. [#410751]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[] {"test1", "test2"}, "test");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
